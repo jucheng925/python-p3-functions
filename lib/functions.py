0da1dc7d769f935f7ecfd63e4b02c09b0ebf6d85
Julie C <jucheng925@gmail.com>
2023-09-14 23:02:07 -0400

    pass test

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+# def halve(number):
+#     if number != int(number)) or (int(number) != float):
+#         print("fail")
+#     else:
+#         print(number / 2)
+# print(halve(10))
 
 def halve(number):
-    pass
+    return number / 2
+
+print(halve(10))
